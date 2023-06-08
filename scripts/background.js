b4e14130abcd451208b6dfec2a42c520d811d7e4
Brandon Wright <psybw7@nottingham.ac.uk>
2023-06-08 19:18:24 +0100

    Del: code cleanup, remove else statement and print

--- scripts/background.js
+++ scripts/background.js
@@ -3,21 +3,21 @@ const YT = "https://www.youtube.com"
 
 
 function foo(){
-    console.log("foo")
-    return document.title}
+
+    return document.title
+
+}
 
 chrome.action.onClicked.addListener(async (tab) => {
 
     if(tab.url.startsWith(YT)){
+
         chrome.scripting.executeScript({
+           
             target: {tabId : tab.id},
             func: foo
 
         }).then(() => console.log("foo"))
-        console.log("Site is YT")
-
-    }else{
-        console.log(`Site is not YT it is: ${tab.url}`)
     }
     
 
