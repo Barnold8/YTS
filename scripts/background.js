275603ca078ccff1b066ecc2fd40f93a7e77dc84
Brandon Wright <psybw7@nottingham.ac.uk>
2023-06-08 19:10:07 +0100

    Add: background.js {site detection}

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,18 @@
 
+const YT = "https://www.youtube.com"
+
+
+
 
 chrome.action.onClicked.addListener(async (tab) => {
 
-    
+    if(tab.url.startsWith(YT)){
+
+        console.log("Site is YT")
+
+    }else{
+        console.log(`Site is not YT it is: ${tab.url}`)
+    }
 
 
 })
