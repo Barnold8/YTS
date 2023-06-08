8fba7daeb392a9bde2ae0391e4e7edc691753868
Brandon Wright <psybw7@nottingham.ac.uk>
2023-06-08 19:17:33 +0100

    Add: basic script injection via test foo function

--- scripts/background.js
+++ scripts/background.js
@@ -2,17 +2,23 @@
 const YT = "https://www.youtube.com"
 
 
-
+function foo(){
+    console.log("foo")
+    return document.title}
 
 chrome.action.onClicked.addListener(async (tab) => {
 
     if(tab.url.startsWith(YT)){
+        chrome.scripting.executeScript({
+            target: {tabId : tab.id},
+            func: foo
 
+        }).then(() => console.log("foo"))
         console.log("Site is YT")
 
     }else{
         console.log(`Site is not YT it is: ${tab.url}`)
     }
-
+    
 
 })
