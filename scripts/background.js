d27d72156ad0cae118bb28bf7fadc8f490214658
Brandon Wright <psybw7@nottingham.ac.uk>
2023-06-09 01:43:06 +0100

    Add: queue sorted

--- scripts/background.js
+++ scripts/background.js
@@ -6,22 +6,27 @@ const RE = "[0-9]{0,2}:[0-9]{1,2}"
 function foo(){
 
     a = document.getElementsByTagName("ytd-playlist-panel-video-renderer")
-    queue = {}
+    queue = []
     
     for(const elem of a){
-        
         // console.log(elem)
         time = elem.innerText.match("[0-9]{0,2}:[0-9]{1,2}")
-        queue[elem.innerText] = {
-            "Element": elem,
-            "time": time
-        }
+        queue.push([time,elem])
     }
 
-    for(var key in queue) {
-        var value = queue[key];
-        console.log(key)
-    }
+    queue.sort(function compare(t1,t2){
+
+        timeLeft = t1[0][0].split(":")
+        timeRight = t2[0][0].split(":")
+
+        timeLeft = parseInt(timeLeft[0])*60 + parseInt(timeLeft[1])
+        timeRight = parseInt(timeRight[0])*60 + parseInt(timeRight[1])
+
+        return timeLeft - timeRight
+
+    })
+
+
     return a
 
 }
