605c95578ff01aea99e0bad2553a19edd8f61aeb
Brandon Wright <psybw7@nottingham.ac.uk>
2023-06-13 02:45:11 +0100

    Add: differentiate between element and psuedoelement

--- scripts/background.js
+++ scripts/background.js
@@ -9,53 +9,18 @@ const RE = "[0-9]{0,2}:[0-9]{1,2}"
 
 function foo(){
 
-    queueContainer = document.getElementsByTagName("ytd-playlist-panel-video-renderer")
-    queue = []
+    queueContainer = document.getElementsByClassName("playlist-items style-scope ytd-playlist-panel-renderer") // get the queue container
+
+    queue = [] // container for sorted elements
+
+    element = queueContainer[1].children[0] // first video of queue
 
     if(queueContainer.length > 1){ // only sort queues longer than 1 elem, useless to sort 1 elem queue
-
-        for(const elem of queueContainer){
-            time = elem.innerText.match("[0-9]{0,2}:[0-9]{1,2}")
-            queue.push([time,elem])
-        }
-        
-        // Uncomment after adding video testing
-        // while(queueContainer.length > 0){ // For some reason, removing elems in for loop doesnt remove all videos. could be a synchronous error
-        //     queueContainer[0].remove()
-        // }
-    
-        queue.sort(function compare(t1,t2){
-    
-            timeLeft = t1[0][0].split(":")
-            timeRight = t2[0][0].split(":")
-    
-            timeLeft = parseInt(timeLeft[0])*60 + parseInt(timeLeft[1])
-            timeRight = parseInt(timeRight[0])*60 + parseInt(timeRight[1])
-    
-            return timeLeft - timeRight
-    
-        })
-    
-        console.log("========================================")
-        console.log(queue)
-        console.log("========================================")
-    
-        console.log(`Queue length ${queue.length}`)
-    
-        x = document.getElementsByClassName("playlist-items style-scope ytd-playlist-panel-renderer")
-        // x[1].innerHTML = "<p>helloooo</p>"
-        for(let i = 0; i < queue.length; i++){
-            node = document.createElement('ytd-playlist-panel-video-renderer')
-            node.innerHTML = queue[i]
-            x[1].appendChild(node)
-        }
-       
+      //ytd-playlist-panel-video-renderer
+      addElement = document.createElement('ytd-playlist-panel-video-renderer');
+      queueContainer[1].appendChild(element)
+      
     }
-
-
-
-    
-
 
 
 }
