15ab75f5abf237e4cdd3c0ca670697f5dd1801bc
Brandon Wright <psybw7@nottingham.ac.uk>
2023-06-17 18:31:27 +0100

    Add: boilerplate html adding code

--- scripts/popup.js
+++ scripts/popup.js
@@ -4,7 +4,12 @@ function generateQueue(){
     chrome.runtime.sendMessage({
         msg: "generateQueue", 
     });
+    const para = document.createElement("p");
+    const node = document.createTextNode("This is a test");
+    para.appendChild(node);
 
+    const element = document.getElementById("fooDiv");
+    element.appendChild(para);
 }
 
 document.getElementById("generateQueue").addEventListener("click", generateQueue);
