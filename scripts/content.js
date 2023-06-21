04c0eec172cf11d2e4d09ad54dca7b506493103b
Brandon Wright <psybw7@nottingham.ac.uk>
2023-06-21 21:36:09 +0100

    Add: grab image link from metadata

--- scripts/content.js
+++ scripts/content.js
@@ -2,6 +2,13 @@
 //TODO: 
     // error handling for no video queues available
 
+function grabLink(text){
+
+        var urlRegex = /(https?:\/\/[^\s]+)/g;
+        var link = text.match(urlRegex);
+        return link
+}
+
 function processQueue(){
 
     queueContainer = document.getElementsByTagName("ytd-playlist-panel-video-renderer")
@@ -10,21 +17,27 @@ function processQueue(){
     if(queueContainer.length > 1){ // only sort queues longer than 1 elem, useless to sort 1 elem queue
 
         for(const elem of queueContainer){
-            console.log(elem)
+            // console.log(elem)
             console.log(elem.childNodes)
+            // console.log(elem.childNodes.getElementsByTagName("img"))
 
             meta_time = elem.innerText.match("[0-9]{0,2}:[0-9]{1,2}").toString()
             
             // seems to be the only way to get the title for a video element. YT, please don't change how your html is structured PLEASE 
             // possible there is some better way to parse these elements?
-            meta_title = elem.childNodes[2].childNodes[1].childNodes[5].innerText 
+            meta_title = elem.childNodes[2].childNodes[1].childNodes[5].innerText // possibly look into getelementsbytagname or some other premade func
 
             meta_href = elem.childNodes[2].href
             
+            meta_imgLink = elem.childNodes[2].childNodes[1].childNodes[3].childNodes[1].childNodes[2].innerHTML
+
+            meta_imgLink = (grabLink(meta_imgLink))[0].split(">")[0]
+
             queue.push({
                 time: meta_time, 
                 title: meta_title, 
-                href: meta_href
+                href: meta_href,
+                img: meta_imgLink
             })
             
         }
