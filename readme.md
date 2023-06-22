848ad69ea8caabbe8e94006de7b8a04e0c69891d
Brandon Wright <psybw7@nottingham.ac.uk>
2023-06-22 18:41:03 +0100

    Add: provide SVG buttons for queue management user side

--- readme.md
+++ readme.md
@@ -34,7 +34,8 @@ Right now, all you can do is click "generate queue".
 
     * implement error handling for null queue
     * replace "fooDiv" placeholder id with more meaningful ID
-    * display a queue on the popup window
+    * display a queue on the popup window - Partly
     * intercept youtube autoplay video change by redirection 
     * button to clear queue
     * detect youtube queue update, add to extension queue, resort
+    * allow time regex to pick up on hours
--- scripts/popup.js
+++ scripts/popup.js
@@ -34,34 +34,55 @@ function resolveHost(){
 
 function generateVideo(elem){ // add div for the title
 
+  const arrowWidth = 15
+  const arrowHeight = 15
+  const arrowColour = "#FFFFFF"
+
+  const upArrow = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="${arrowColour}" height="${arrowHeight}" width="${arrowWidth}px" version="1.1" id="Layer_1" viewBox="0 0 330 330" xml:space="preserve"><path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394  l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393  C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"/></svg>`
+  const downArrow = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="${arrowColour}" height="${arrowHeight}" width="${arrowWidth}px" version="1.1" id="Layer_1" viewBox="0 0 330 330" xml:space="preserve"><path id="XMLID_225_" d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393  c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393  s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"/></svg>`
+
   const video = document.createElement("li")
   const imageDiv = document.createElement("div")
-  const timeDiv = document.createElement("div")
+  const time = document.createElement("div")
   const image = document.createElement("img")
+  const titleDiv = document.createElement("div")
   const title = document.createElement("p")
   const link  = document.createElement("a") // more used to actually store the link in an elem rather than for user side use
-  const time  = document.createElement("p")
+  const buttonContainer = document.createElement("div")
+  const down  = document.createElement("div")
+  const up  = document.createElement("div")
 
   link.href = elem["href"]
   title.innerText = elem["title"]
-  timeDiv.innerText = elem["time"]
+  time.innerText = elem["time"]
   image.src = elem["img"]
+  down.innerHTML = downArrow
+  up.innerHTML = upArrow
   
   title.appendChild(link)
 
-  video.classList.add("queueVideo") 
+  video.classList.add("queueContainer") 
   title.classList.add("title")
   image.classList.add("thumbnail")
   imageDiv.classList.add("thumbnailContainer")
-  timeDiv.classList.add("timeContainer")
+  time.classList.add("timeContainer")
+  titleDiv.classList.add("titleContainer")
+  buttonContainer.classList.add("videoFuncContainer")
+  down.classList.add("videoFunc")
+  up.classList.add("videoFunc")
 
   imageDiv.appendChild(image)
-  // timeDiv.append(time)
-  imageDiv.appendChild(timeDiv)
+  imageDiv.appendChild(time)
+
+  titleDiv.appendChild(title)
+  titleDiv.appendChild(buttonContainer)
+
+  buttonContainer.appendChild(up)
+  buttonContainer.appendChild(down)
 
   video.appendChild(imageDiv)
-  video.appendChild(title)
-  
+  video.appendChild(titleDiv)
+
 
   // queue.push(video.toString()) // to perserve queue later on
 
@@ -81,7 +102,6 @@ function generateQueue(){
   (async () => {
     const tabs = await chrome.tabs.query({currentWindow: true, active: true});
     const response = await chrome.tabs.sendMessage(tabs[0].id, {type: "getInitialQueue"});
-
 
     if(response.payload === null){
       const node = document.createElement("div");
