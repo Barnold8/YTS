4c92935ec408dab866e727507b98e22192910b20
Brandon Wright <psybw7@nottingham.ac.uk>
2023-06-25 20:27:47 +0100

    Add: queue changes are preserved in memory for manual resorting

--- scripts/popup.js
+++ scripts/popup.js
@@ -79,7 +79,28 @@ function swapVideo(evt){
                       element.addEventListener('click', swapVideo);
                       element.func_param = element.id
                 });
-          
+                chrome.storage.session.get(["queueInfo"]).then((result) => { // reinit the queue 
+              
+                  videoOne = result.queueInfo[0]["videoQueue"][parseInt(videoID)]
+                  videoTwo = result.queueInfo[0]["videoQueue"][parseInt(videoID)-1]
+                
+                  videoOne["videoID"] = parseInt(videoOne["videoID"]) - 1
+                  videoTwo["videoID"] = parseInt(videoTwo["videoID"]) + 1
+                  
+                  result.queueInfo[0]["videoQueue"][parseInt(videoID)] = videoTwo
+                  result.queueInfo[0]["videoQueue"][parseInt(videoID)-1] = videoOne
+                  
+                  console.log(result.queueInfo[0]["videoQueue"])
+  
+                  chrome.storage.session.set({
+                      queueInfo:[{    
+                                      intialQueue: result.queueInfo[0]["intialQueue"],
+                                      videoQueue: result.queueInfo[0]["videoQueue"]
+                                }]
+                  }).then(() => {
+                                  console.log("Initial queue has been set");
+                  });
+            });
 
           }
         break; 
@@ -95,6 +116,7 @@ function swapVideo(evt){
                     element.addEventListener('click', swapVideo);
                     element.func_param = element.id
               });
+              
               chrome.storage.session.get(["queueInfo"]).then((result) => { // reinit the queue 
               
                 videoOne = result.queueInfo[0]["videoQueue"][parseInt(videoID)]
